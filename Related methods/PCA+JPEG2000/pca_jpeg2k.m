d80e657cbc9b631f3ac5bff1e64d8689920e0ddb
Z.LS <100690601+ANIMZLS@users.noreply.github.com>
2025-04-02 16:15:59 +0800

    Update

--- Related methods/PCA+JPEG2000/pca_jpeg2k.m
+++ Related methods/PCA+JPEG2000/pca_jpeg2k.m
@@ -10,17 +10,13 @@ IMAGE_INTEGER_REPRESENTATION = 'uint16';
 originalImage = single(inputImage);
 normalizedImage = originalImage / NUM_TONES;
 
-% 将三维数组转换为二维数组
 [num_pixels, num_bands] = size(normalizedImage);
 normalizedImage = reshape(normalizedImage, num_pixels, num_bands);
 
-% 设置块的大小
 block_size = 256;
 
-% 获取块的数量
 num_blocks = num_pixels / block_size;
 
-% 初始化结果数组
 reconstructedImage_blocks = zeros(size(normalizedImage), 'single');
 
 % PCA和MinMaxScaler初始化
@@ -37,26 +33,20 @@ for i = 1:num_blocks
     end_idx = i * block_size;
     block_data = normalizedImage(start_idx:end_idx, :);
 
-    % 压缩
     reducedImage_block = pca.transform(block_data);
     JPEG2000Encoding_block = imencode('.jpg', mms.fit_transform(reducedImage_block), 'jpg');
 
-    % 解压缩
     JPEG2000Decoding_block = mms.inverse_transform(imdecode(JPEG2000Encoding_block, 'jpg'));
     reconstructedImage_block = pca.inverse_transform(JPEG2000Decoding_block) * NUM_TONES;
 
-    % 将块的结果存储到结果数组中
     reconstructedImage_blocks(start_idx:end_idx, :) = reconstructedImage_block;
 end
 
-% 将结果数组还原回原始形状
 reconstructedImage = reshape(reconstructedImage_blocks, size(normalizedImage));
 
-% 将二维数组还原为三维数组
 restoredImage = reshape(reconstructedImage, size(originalImage));
 end_time = toc(start_time);
 
-% 假设 `reconstructedImage` 是解压缩后的数据
 data_to_save = struct('pca_jpeg2k', restoredImage);
 save('REC/pca+jpeg2000(U).mat', '-struct', 'data_to_save');
 
@@ -72,7 +62,7 @@ disp(['PSNR: ', num2str(psnr_value)]);
 disp(['总执行时间: ', num2str(total_time), ' 秒']);
 
 % 计算压缩比
-original_size = numel(inputImage) * 4; % 假设使用单精度浮点数（4字节每个元素）
+original_size = numel(inputImage) * 4;
 compressed_size = sum(cellfun('length', JPEG2000Encoding_block));
 compression_ratio = original_size / compressed_size;
 
